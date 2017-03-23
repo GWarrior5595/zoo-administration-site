ed6554306506fd61aa92404569252ee38ff5fadd
Erphun Ranjbar <erphunranjbar@Erphuns-MacBook-Pro.local>
2017-03-23 14:42:35 -0500

    login authentication works, just need to add error messages

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://GWarrior5595:159753Er@ds139430.mlab.com:39430/zoo_admin_users' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
--- index.js
+++ index.js
@@ -1,38 +1,49 @@
-var express = require("express");
 var engines = require('consolidate');
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
 var app = express();
-//var mysql = require('mysql');
-
-// var connection = mysql.createConnection({
-//   host: 'us-cdbr-iron-east-03.cleardb.net',
-//   user: 'b3a58478f963a6',
-//   password: 'e4081158',
-//   database: 'heroku_db65f8e9326be4b'
-// });
-
-app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views/pages');
 app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
-//connection.connect();
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-app.get('/', function(request, response) {
-  response.render('pages/login');
-  // connection.query('SELECT * from animals', function(err, rows, fields) {
-  //     if (err) {
-  //       console.log('error: ', err);
-  //       throw err;
-  //     }
-  //     response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
-  //   });
+require('./config/passport')(passport); // pass passport for configuration
 
-});
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: 'hamburgers5595', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./public/js/routes.js')(app, passport); // load our routes and pass in our app
+
+app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
