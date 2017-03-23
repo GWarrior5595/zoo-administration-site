80957aa7935149311a8da5b4529a608adde04496
Erphun Ranjbar <erphunranjbar@Erphuns-MacBook-Pro.local>
2017-03-23 00:55:11 -0500

    deployed database to heroku and tested that we are able to fetch data

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var engines = require('consolidate');
 var app = express();
+//var mysql = require('mysql');
+
+// var connection = mysql.createConnection({
+//   host: 'us-cdbr-iron-east-03.cleardb.net',
+//   user: 'b3a58478f963a6',
+//   password: 'e4081158',
+//   database: 'heroku_db65f8e9326be4b'
+// });
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,8 +19,18 @@ app.set('views', __dirname + '/views');
 app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
+//connection.connect();
+
 app.get('/', function(request, response) {
   response.render('pages/index');
+  // connection.query('SELECT * from animals', function(err, rows, fields) {
+  //     if (err) {
+  //       console.log('error: ', err);
+  //       throw err;
+  //     }
+  //     response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+  //   });
+
 });
 
 app.listen(app.get('port'), function() {
