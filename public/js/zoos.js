6b6e5cac11ed45c22a706a650156a90884489c10
Kim Pham <Kim Pham>
2017-04-15 16:59:47 -0500

    deleted useless comment

--- public/js/zoos.js
+++ public/js/zoos.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
                     var id = {
                         'Zoo ID': element['Zoo ID']
                     };
-//flip card
+                                //flip card
                                          document.getElementById("cardsContainer").innerHTML
                                             //back 
                                          += "<div class='flip3D'>"
@@ -34,8 +34,6 @@ $(document).ready(function(){
                                                     + "Zoo name: " + element['Name'] + "</p></div>"
                                          + "</div>"
 
-//samad's
-//                   document.getElementById("cardsContainer").innerHTML += "<div class='col-lg-6 col-sm-6'> <div class='card'><a href='enclosure' class='fill-div'><div class='content'> <div class='row'> <div class='col-xs-5'> <div class='icon-big icon-warning text-center'> <i class='ti-linux'></i> </div> </div> <div class='col-xs-7'> <div class='exhibit'> <p>" + element['Description'] + "</p>  </div> </div> </div> </div> </div></a>
 
                     console.log(element['Name'])
                 }, this)
@@ -44,3 +42,50 @@ $(document).ready(function(){
     }, 300);
 
 });
+
+
+
+
+
+
+
+
+
+//add new zoo
+function insertZoo(){
+    var enclosure = document.getElementById("enclosure");
+    var shop = document.getElementById("shop");
+    var today = new Date();
+
+    var nullValue = null;
+
+
+   
+        entry = {
+            'Zoo ID': 1,
+            'First Name': $("#firstName").val(),
+            'Last Name': $("#lastName").val(),
+            'Enclosure ID': enclosure.options[enclosure.selectedIndex].id,
+            'Shop ID': shop.options[shop.selectedIndex].id,            
+            'Job Desciption': $("#jobDescription").val(),
+            'Hire Date': today,
+            'Shifts': $("#shift").val(),
+            'Salary': salaryMinimum
+        };
+    
+
+    $.ajax({
+        url: "/addZoo",
+        type: "POST",
+        contentType: "application/json",
+        processData: false,
+        data: JSON.stringify(entry),
+        complete: function (data) {
+            $('#output').html(data.responseText);
+            //reloadEmployeeTable();
+        }
+    });
+
+    $("#dialog-form-insert").dialog("close");    
+    document.getElementById('topPage').scrollIntoView();
+}
